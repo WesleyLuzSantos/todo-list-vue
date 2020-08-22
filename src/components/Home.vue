edf1d9131044b3765d6659f987aa9ae41c8ed90d
Wesleyluzsantos <santosluzwesley@gmail.com>
2020-08-22 09:14:32 -0400

    Segundo commit

---
+++ src/components/Home.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        <v-card>
+            <v-card-title>Título do Card</v-card-title>
+            <v-card-text>
+                Texto do card
+            </v-card-text>
+        </v-card>
+    </div>
+</template>
